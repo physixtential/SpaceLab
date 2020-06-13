cb2444be3984938dc048768868ad23c5b593a356
BuildTools <unconfigured@null.spigotmc.org>
2020-06-12 23:46:32 -0700

    added y and z kick dimenations

--- Collider/ColliderOMP.cpp
+++ Collider/ColliderOMP.cpp
@@ -76,8 +76,8 @@ int main(int argc, char const* argv[])
 			fprintf(stderr, "A VELOCITY WAS NAN!!!!!!!!!!!!!!!!!!!!!!\n\n");
 			exit(EXIT_FAILURE);
 		}
-		clusA.kick(vSmall);
-		clusB.kick(vBig);
+		clusA.kick(vSmall, .0, .0);
+		clusB.kick(vBig, .0, .0);
 		clusA.checkMomentum();
 		clusB.checkMomentum();
 		cosmos.balls.insert(cosmos.balls.end(), clusA.balls.begin(), clusA.balls.end());
