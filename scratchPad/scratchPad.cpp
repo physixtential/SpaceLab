c573eaa4009b1bf6c68ebacbce4f909b25a1ccc7
Job <physixtential@gmail.com>
2021-06-28 13:36:32 -0700

    Added cuda project

--- scratchPad/scratchPad.cpp
+++ scratchPad/scratchPad.cpp
@@ -9,15 +9,33 @@
 #include <unordered_map>
 #include <windows.h>
 
-void changeArr(float* arr)
+
+
+vector3d* g_positions; // one position per particle
+vector3d* g_accelerationTotals; // one acceleration total per particle
+
+void calculate(int particleAidx, int particleBidx)
 {
-	arr[0] = 1;
-	arr[1] = 2;
-	arr[2] = 3;
+    vector3d particleAposition = g_positions[particleAidx];
+    vector3d particleBposition = g_positions[particleBidx];
+    //...
+    vector3d acceleration = 0;// todo, acceleration between particle A and B
+
+    // can't do this, not multi-thread safe
+    //g_accelerationTotals[particleAidx] += acceleration;
+    //g_accelerationTotals[particleBidx] -= acceleration;
+
+    // add the acceleration of A and B to their totals, this IS multi-thread safe
+    InterlockedAdd(&g_accelerationTotals[particleAidx].x, acceleration.x);
+    InterlockedAdd(&g_accelerationTotals[particleAidx].y, acceleration.y);
+    InterlockedAdd(&g_accelerationTotals[particleAidx].z, acceleration.z);
+    InterlockedAdd(&g_accelerationTotals[particleAidx].x, -acceleration.x);
+    InterlockedAdd(&g_accelerationTotals[particleAidx].y, -acceleration.y);
+    InterlockedAdd(&g_accelerationTotals[particleAidx].z, -acceleration.z);
 }
 
 
 int main()
 {
-	cout << !true;
+	
 }
