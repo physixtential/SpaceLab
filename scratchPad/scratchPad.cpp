8bee0a0c2ed7ae059e80368287575ad3918c5f71
Job <physixtential@gmail.com>
2021-07-21 12:54:30 -0700

    trying to reset master

--- scratchPad/scratchPad.cpp
+++ scratchPad/scratchPad.cpp
@@ -1,41 +1,21 @@
 #include "scratchPad.h"
 
-#include <ppl.h>
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <unordered_map>
-#include <windows.h>
-
-
-
-vector3d* g_positions; // one position per particle
-vector3d* g_accelerationTotals; // one acceleration total per particle
-
-void calculate(int particleAidx, int particleBidx)
+int add_three(int x, int y)
 {
-    vector3d particleAposition = g_positions[particleAidx];
-    vector3d particleBposition = g_positions[particleBidx];
-    //...
-    vector3d acceleration = 0;// todo, acceleration between particle A and B
-
-    // can't do this, not multi-thread safe
-    //g_accelerationTotals[particleAidx] += acceleration;
-    //g_accelerationTotals[particleBidx] -= acceleration;
-
-    // add the acceleration of A and B to their totals, this IS multi-thread safe
-    InterlockedAdd(&g_accelerationTotals[particleAidx].x, acceleration.x);
-    InterlockedAdd(&g_accelerationTotals[particleAidx].y, acceleration.y);
-    InterlockedAdd(&g_accelerationTotals[particleAidx].z, acceleration.z);
-    InterlockedAdd(&g_accelerationTotals[particleAidx].x, -acceleration.x);
-    InterlockedAdd(&g_accelerationTotals[particleAidx].y, -acceleration.y);
-    InterlockedAdd(&g_accelerationTotals[particleAidx].z, -acceleration.z);
+	return x + y;
 }
-
 
 int main()
 {
-	
+	std::vector<int> a = { 1, 2, 3, 4, 5 };
+	std::vector<int> b = { 5, 4, 3, 2, 1 };
+	std::vector<int> c(5);
+
+	std::transform(a.begin(), a.end(), b.begin(), c.begin(), add_three);
+
+	for (size_t i = 0; i < c.size(); i++)
+	{
+		std::cout << a[i] << " + " << b[i] << " = " << c[i] << '\n';
+
+	}
 }
