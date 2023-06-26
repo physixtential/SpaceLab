40df66aed1f825f7a1ff91924b3293c4765bd981
LucasKolanz <kolanzl@oregonstate.edu>
2023-06-26 10:47:04 -0700

    it runs

--- default_files/run_sim.py
+++ default_files/run_sim.py
@@ -16,8 +16,8 @@ def main():
 	# out = os.system("./ColliderSingleCore.o {}".format(curr_folder))
 	# out = os.system("./ColliderSingleCore.o {} 1>> {} 2>> {}".format(curr_folder,output_file,error_file))
 	
-	cmd = ["srun","-n","1","-c","2","{}ColliderSingleCore.x".format(location), location, str(num_balls)]
-	# cmd = ["{}ColliderSingleCore.o".format(location), location, str(num_balls)]
+	# cmd = ["srun","-n","1","-c","2","{}ColliderSingleCore.x".format(location), location, str(num_balls)]
+	cmd = ["{}ColliderSingleCore.x".format(location), location, str(num_balls)]
 	with open(output_file,"a") as out, open(error_file,"a") as err:
 		subprocess.run(cmd,stdout=out,stderr=err)
 
--- make_test_job.py
+++ make_test_job.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
 	runs_at_once = 1
 	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
-	attempts = [3] 
+	attempts = [5] 
 	N = [100]
 	Temps = [100]
 	folders = []
@@ -65,7 +65,7 @@ if __name__ == '__main__':
 
 				#add run script and executable to folders
 				os.system("cp default_files/run_sim.py {}run_sim.py".format(job))
-				os.system("cp ColliderSingleCore/ColliderSingleCore.o {}ColliderSingleCore.o".format(job))
+				os.system("cp ColliderSingleCore/ColliderSingleCore.x {}ColliderSingleCore.x".format(job))
 				folders.append(job)
 	# print(folders)
 	if len(N) != len(folders):
