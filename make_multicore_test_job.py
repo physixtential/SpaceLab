08670582f67073fda660e93bbe1c16e0ee7b550e
LucasKolanz <luko8622@colorado.edu>
2023-07-10 10:19:39 -0700

    Debugging unphysical angular acceleration.

--- make_multicore_test_job.py
+++ make_multicore_test_job.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
 	runs_at_once = 1
 	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
-	attempts = [10] 
+	attempts = [1] 
 	N = [30]
 	Temps = [100]
 	folders = []
