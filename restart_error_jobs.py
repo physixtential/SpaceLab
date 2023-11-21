4f0620788ad2dd9c0a250e426ab412a61f49f09e
LucasKolanz <kolanzl@oregonstate.edu>
2023-11-20 17:58:33 -0800

    restart error code moves error folder for preservation.

--- restart_error_jobs.py
+++ restart_error_jobs.py
@@ -14,22 +14,43 @@ import subprocess
 import check_for_errors as cfe
 
 
-def restart_job(folder,test=True,remove_only=False):
-	if test:
-		command = "ls"
-	else:
-		command = "rm"
-	cwd = os.getcwd()
-	os.chdir(folder)
-	os.system(f"{command} *.csv")
-	os.system(f"{command} *.txt")
-	os.system(f"{command} *.x")
-	os.system(f"{command} *.py")
-	os.system(f"{command} *.cpp")
-	os.system(f"{command} *.hpp")
-	os.chdir(cwd)
+def restart_job(folder,test=True,move_folder=''):
 
-	if not remove_only:
+	if len(move_folder) > 0 #move data to new folder specified in move_folder
+		if os.path.exists(move_folder): #if move_folder already exists
+			#if it already exists then we need to change the name of it so it doesn't overwrite
+			move_folder += '_MOVE-'
+			move_index = 0
+			while os.path.exists(move_folder+str(move_index)): #check for lowest number transpher. 
+				move_index += 1
+
+			move_folder += str(move_index) + '/'
+			os.makedirs(move_folder)
+		
+		else : #make it
+			os.makedirs(move_folder)
+
+		command = f"mv {folder}* {move_folder}."
+		if test:
+			print(command)
+		else:
+			os.system(command)
+	else: #remove and restart
+		cwd = os.getcwd()
+		os.chdir(folder)
+		if test:
+			command = "ls"
+		else:
+			command = "rm"
+
+		os.system(f"{command} *.csv")
+		os.system(f"{command} *.txt")
+		os.system(f"{command} *.x")
+		os.system(f"{command} *.py")
+		os.system(f"{command} *.cpp")
+		os.system(f"{command} *.hpp")
+		os.chdir(cwd)
+
 		try:
 			# os.chdir("{}ColliderSingleCore".format(curr_folder))
 			subprocess.run(["make","-C","ColliderSingleCore"], check=True)
@@ -37,7 +58,6 @@ def restart_job(folder,test=True,remove_only=False):
 			print('compilation failed')
 			print(e)
 			exit(-1)
-
 
 		if not test:
 			os.system("cp default_files/run_sim.py {}run_sim.py".format(folder))
@@ -58,7 +78,8 @@ def main():
 
 	curr_folder = os.getcwd() + '/'
 
-	job = curr_folder + 'jobsCosine/lognorm$a$/N_$n$/T_$t$/'
+	job = curr_folder + 'jobs/lognorm$a$/N_$n$/T_$t$/'
+	move_folder = curr_folder + 'erroredJobs/lognorm$a$/N_$n$/T_$t$/'
 
 
 
@@ -78,7 +99,7 @@ def main():
 	error2_folders = cfe.check_error(job,cfe.error2,N,Temps,attempts)
 	for folder in error2_folders:
 		# print(folder)
-		restart_job(folder,test=True,remove_only=True)
+		restart_job(folder,test=True,move_folder=move_folder)
 		# exit(0)
 	# print(error2_folders)
 
